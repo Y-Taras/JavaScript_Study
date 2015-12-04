dab9cf781644cc08f6fed792c7445857dc801342
Masha <m.diminsky@gmail.com>
2015-12-04 17:20:52 +0700

    Create LetterCapitalize.js

---
+++ Coderbyte/Easy/LetterCapitalize.js
@@ -0,0 +1,25 @@
+/* 
+  Challenge:
+  Using the JavaScript language, have the function LetterCapitalize(str) take 
+  the str parameter being passed and capitalize the first letter of each word. 
+  Words will be separated by only one space. 
+  
+  Challenge: ACCEPTED.
+    * Split the string into words(assuming it's a sentence).
+    * Loop through each word.
+    * Using charAt() method, print the first letter and capitalize it and join it 
+      with the rest of the word, except the first letter with the slice() method.
+*/
+
+
+function LetterCapitalize(str) { 
+  var words=str.split(" ");
+  var newArrString=[];
+  
+  for(var i=0; i<words.length;i++) {
+    newArrString.push(words[i]=words[i].charAt(0).toUpperCase() + words[i].slice(1));
+  }
+  
+  return newArrString.join(" ");
+}
+
