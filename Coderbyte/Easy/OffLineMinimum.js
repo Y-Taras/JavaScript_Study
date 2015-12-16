23cf84e5bc72e5699de800fa2e37d04c31c1a0b1
Masha <m.diminsky@gmail.com>
2015-12-16 17:55:31 +0700

    Create OffLineMinimum.js

---
+++ Coderbyte/Easy/OffLineMinimum.js
@@ -0,0 +1,20 @@
+/*
+Challenge:
+Have the function OfflineMinimum take the StrArr parameter being
+passed which will be an array of integers raning from ...n and the
+letter 'E' and return the subset based on the following rules. The
+StrArr will be formatted as [i,i,i,E,i,E] where i stands for an 
+integer and 'E' stands for the letter E. Whenever the value of an
+index is E, return the lowest integer in the array. For example, 
+if StrArr is ["5","4","6","E","1","7","E","E","3","2"] it should 
+return 4,1,5. 
+
+In progress: http://jsfiddle.net/1nmzd0x7/28/
+*/
+
+
+
+
+
+alert(OffLineMinimum(["5","4","6","E","1","7","E","E","3","2"])); // 4,1,5
+
