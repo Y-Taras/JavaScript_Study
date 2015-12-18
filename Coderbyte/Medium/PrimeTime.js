302550196bf763d837da5e809247961ff57e2c62
Masha <m.diminsky@gmail.com>
2015-12-18 14:48:34 +0700

    Update PrimeTime.js

--- Coderbyte/Medium/PrimeTime.js
+++ Coderbyte/Medium/PrimeTime.js
@@ -20,9 +20,9 @@
 */
 
 function PrimeTime(num){
-  // num divides by numbers between 2-9
+  // num divides by numbers between 2-9.
   for(var i=2; i<10; i++){
-    // if result is whole number and the number doesn't divide by itself.
+    // if result is whole number and num doesn't divide by itself.
     if(num/i === Math.round(num/i) && num !==i){ 
       return false;
     }
