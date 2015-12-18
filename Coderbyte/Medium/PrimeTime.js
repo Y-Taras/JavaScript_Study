496b89b8d392ad9e5a64c09a2c9a1825d94acd89
Masha <m.diminsky@gmail.com>
2015-12-18 14:47:25 +0700

    Update PrimeTime.js

--- Coderbyte/Medium/PrimeTime.js
+++ Coderbyte/Medium/PrimeTime.js
@@ -1,9 +1,9 @@
 /*
   Challenge:
-   Using the JavaScript language, have the function PrimeTime(num) 
-   take the num parameter being passed and return the string true 
-   if the parameter is a prime number, otherwise return the string 
-   false. The range will be between 1 and 2^16 (65536).
+  Using the JavaScript language, have the function PrimeTime(num) 
+  take the num parameter being passed and return the string true 
+  if the parameter is a prime number, otherwise return the string 
+  false. The range will be between 1 and 2^16 (65536).
 
   Challenge: ACCEPTED.
   Tips: 
