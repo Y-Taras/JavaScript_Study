76cc0a51be72ca7aed523755c9501fb8cb92ac95
Masha <m.diminsky@gmail.com>
2016-01-03 13:14:04 +0700

    Update and rename FirstFactorial.js to 02_FirstFactorial.js

--- Coderbyte/Easy/02_FirstFactorial.js
+++ Coderbyte/Easy/02_FirstFactorial.js
@@ -5,15 +5,19 @@
   For the test cases, the range will be between 1 and 18. 
 
   Challenge: ACCEPTED.
-  * Loop through numbers starting with 1(since we're not multiplying by 0) until num.
-  * Multiply each result with the preceding number until reach greater than num.
+  * Loop through and mutiply numbers starting with 1(since we're not multiplying by 0) until num.
 */
 
 function FirstFactorial(num) {
   var newNum=1;
-  for(var i=1; i<=num ;i++){
+  
+  for(var i=1; i<=num ;i++) {
     newNum*=i;
   }
   	
   return newNum;         
 }
+
+// TESTS
+console.log(FirstFactorial(4)); // 24
+console.log(FirstFactorial(8)); // 40320
